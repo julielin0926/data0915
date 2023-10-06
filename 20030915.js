8bac37ccf1be0af4772ea3a9024067e7ac62c528
Laiii1017 <145099992+Laiii1017@users.noreply.github.com>
2023-10-06 16:08:01 +0800

    data0915

---
+++ 20030915.js
@@ -0,0 +1,7 @@
+var A = Math.floor (Math.random()*11);
+console.log (A);
+var readline = require("readline-sync");
+
+while(true){
+var guess = readline.question("please a number between 0 and 10");
+}
